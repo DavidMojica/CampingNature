03007b94416002363e76783568c9844c85af853c
David Mojica <davidmojicav@gmail.com>
2023-02-12 18:35:37 -0500

    Index 80% ended.

---
+++ scripts/index_elements.js
@@ -0,0 +1,41 @@
+//GLOBAR VARIABLES
+var sec = document.getElementById('sec');      //<-middle DIV
+var image_list = ['../media/login_img.png','../media/register_img.png','../media/catalog_img.png', '../media/contact_img.png']; 
+var h2text_list = ['Login', 'Register', 'Products', 'Contact Us'];
+var a_href_list = ['#','#','#','#']; //<-When another pages 
+var amount_of_bubbles = 15; //<--- Amount of bubbles | Very high quantities can cause malfunctions
+//Functions & arrows
+var RandomPositiveInt =(max)=> { return Math.floor(Math.random() * max);}  // Get random int above 0 (0 included) & under max(max not included). If radomInt receive (3) -> Expected output: 0, 1 or 2
+var RandomSign =(rands) => { if(rands%2==0) return '+'; else return '-';}  //Get one or both signs randomly. 
+var getRandomInt = (min, max)=> { min = Math.ceil(min); max = Math.floor(max); //Get a random number above min(included) & under the max (not included).
+    return Math.floor(Math.random() * (max - min) + min);        // The maximum is exclusive and the minimum is inclusive
+}
+//-----------BUBBLES CONSTRUCTION-----------//
+for (let i=0; i<amount_of_bubbles;i++){
+    let weightHeight = RandomPositiveInt(200);   //<------Maxium extent of each bubble
+    let bubble = document.createElement('div');  
+    bubble.style.backgroundColor = 'rgb(46, 141, 185)';
+    bubble.style.borderRadius = '100%';
+    bubble.style.position = 'absolute';
+    bubble.style.bottom = '0';
+    bubble.style.animation = 'animarburbujas 4s linear infinite';
+    bubble.style.zIndex = RandomPositiveInt(2);
+    bubble.style.width = weightHeight+"px";
+    bubble.style.height = weightHeight+"px";
+    bubble.style.left = `${RandomPositiveInt(93)}%`; //<----Maxium spawn point of each bubble. Max recomended: 93% for avoid overflow and override disasters :D
+    bubble.style.animationDuration = `${getRandomInt(5,20)}s`;
+    sec.appendChild(bubble);}
+//------------BUTTONS CONSTRUCTION-----------//
+for(let i=0; i<4;i++){
+    let btnx = document.createElement('button'); 
+    let a = document.createElement('a');
+    let h2 = document.createElement('h2');
+    let h2text = document.createTextNode(`${h2text_list[i]}`); 
+    btnx.setAttribute('class','nav_button');                    
+    a.setAttribute('href',`${a_href_list[i]}`);
+    btnx.style.background = `rgba(51, 51, 0, 0.5) url(${image_list[i]})  no-repeat`;
+    btnx.style.backgroundSize = '100% 70%';
+    btnx.appendChild(h2);
+    h2.appendChild(h2text);
+    a.appendChild(btnx);
+    document.getElementById('sec').appendChild(a); }
---
+++ scripts/index_forest_animation.js
@@ -0,0 +1,12 @@
+window.addEventListener('scroll',function(){                      //<---Scroll event handler.
+    let value = window.scrollY;  //<----Scroll down gains value points (in px). Scroll up misses value points.
+    document.getElementById('text').style.top= 50+value*-0.5+'%';   //<---text delimited their top side.
+    document.getElementById('bird1').style.top = value*-1.5+'px';   //<---birds delimits top & left side. Birds will move in 45deg from their respective sides.
+    document.getElementById('bird1').style.left = value*2+'px';
+    document.getElementById('bird2').style.top = value*-1.5+'px';
+    document.getElementById('bird2').style.left = value*-5+'px';
+    document.getElementById('btn').style.marginTop = value * 1.5+'px'; //<-----Generates margin in top side.
+    document.getElementById('rocks').style.top = value * 0.12 + 'px'; //<----Like text comment.
+    document.getElementById('forest').style.top = value *0.25+'px';
+    document.getElementById('header').style.top = value*0.5+'px';
+})
